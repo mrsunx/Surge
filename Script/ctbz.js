ce804f907455be04b5c317f117e678ad679a9869
mrxuan <mrxuan.zhang@gmail.com>
2024-07-09 10:57:15 +0800

    Update ctbz.js

--- Script/ctbz.js
+++ Script/ctbz.js
@@ -1,11 +1,12 @@
-
 function addAuth(obj, key) {
   if (obj[key] === undefined || !obj[key]) {
     obj[key] = {
-      allAuthority: true
+      allAuthority: true,
+      vipType: "diamond"
     }
   } else {
     obj[key].allAuthority = true
+    obj[key].vipType = "diamond"
   }
 }
 
@@ -14,13 +15,12 @@ function supplementWithEmptyObject(obj, key) {
       obj[key] = {};
   }
 }
-
 var response = JSON.parse($response.body);
 for (let item of response.data.item || []) {
   addAuth(item, "vipInfo")
 }
 addAuth(response.data, "liuQinTuShi")
-// addAuth(response.data.liuQinTuShi, "vipInfo")
+addAuth(response.data.liuQinTuShi, "vipInfo")
 addAuth(response.data, "baZiGeJuInfo")
 addAuth(response.data.baZiGeJuInfo, "vipInfo")
 addAuth(response.data, "gongWeiTuShi")
@@ -28,7 +28,7 @@ addAuth(response.data, "gongWeiTuShi")
 addAuth(response.data, "tiaoHouYongShen")
 // addAuth(response.data.tiaoHouYongShen, "vipInfo")
 addAuth(response.data, "LiuTongTuShi")
-// addAuth(response.data.LiuTongTuShi, "vipInfo")
+addAuth(response.data.LiuTongTuShi, "vipInfo")
 addAuth(response.data, "chuShengTianTiTu")
 addAuth(response.data, "xianTianTiZhi")
 addAuth(response.data, "riZhuShuXing")
@@ -40,6 +40,5 @@ addAuth(response.data, "xiJiYongShen")
 addAuth(response.data, "daYunCaiFu")
 addAuth(response.data, "caiNianTuBiao")
 addAuth(response.data, "xianTianCaiFu")
-addAuth(response, "data")
 
 $done({body : JSON.stringify(response)});
