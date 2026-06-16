1bc4f9bd7758cdc98279a9e3d70408661f14bab6
rain <mrxuan.zhang@gmail.com>
2026-06-16 15:46:01 +0800

    Update ctbz.js

--- Script/ctbz.js
+++ Script/ctbz.js
@@ -1,44 +1,11 @@
-function addAuth(obj, key) {
-  if (obj[key] === undefined || !obj[key]) {
-    obj[key] = {
-      allAuthority: true,
-      vipType: "diamond"
-    }
-  } else {
-    obj[key].allAuthority = true
-    obj[key].vipType = "diamond"
-  }
-}
-
-function supplementWithEmptyObject(obj, key) {
-  if (obj[key] === undefined) {
-      obj[key] = {};
-  }
-}
 var response = JSON.parse($response.body);
-for (let item of response.data.item || []) {
-  addAuth(item, "vipInfo")
-}
-addAuth(response.data, "liuQinTuShi")
-addAuth(response.data.liuQinTuShi, "vipInfo")
-addAuth(response.data, "baZiGeJuInfo")
-addAuth(response.data.baZiGeJuInfo, "vipInfo")
-addAuth(response.data, "gongWeiTuShi")
-// addAuth(response.data.gongWeiTuShi, "vipInfo")
-addAuth(response.data, "tiaoHouYongShen")
-// addAuth(response.data.tiaoHouYongShen, "vipInfo")
-addAuth(response.data, "LiuTongTuShi")
-addAuth(response.data.LiuTongTuShi, "vipInfo")
-addAuth(response.data, "chuShengTianTiTu")
-addAuth(response.data, "xianTianTiZhi")
-addAuth(response.data, "riZhuShuXing")
-addAuth(response.data, "wuXingGeShu")
-addAuth(response.data, "wuXingHanCangGan")
-addAuth(response.data, "wuXingNengLiang")
-addAuth(response.data, "wangShuai")
-addAuth(response.data, "xiJiYongShen")
-addAuth(response.data, "daYunCaiFu")
-addAuth(response.data, "caiNianTuBiao")
-addAuth(response.data, "xianTianCaiFu")
 
-$done({body : JSON.stringify(response)});
+response.data = response.data || {};
+response.data.vipInfo = response.data.vipInfo || {};
+
+response.data.vipInfo.allAuthority = true;
+response.data.vipInfo.vipType = "diamond";
+
+$done({
+  body: JSON.stringify(response)
+});
